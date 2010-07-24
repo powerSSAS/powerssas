2651bc05db91078022eb319c138ccb1f471003a7
dgosbell_cp <dgosbell@users.noreply.github.com>
2010-07-24 03:10:44 +0000

    Release 0.3.1.1

--- powerSSAS/My Project/AssemblyInfo.vb
+++ powerSSAS/My Project/AssemblyInfo.vb
@@ -32,5 +32,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.3.1.0")> 
-<Assembly: AssemblyFileVersion("0.3.1.0")> 
+<Assembly: AssemblyVersion("0.3.1.1")> 
+<Assembly: AssemblyFileVersion("0.3.1.1")> 
